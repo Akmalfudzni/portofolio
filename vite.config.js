37df89da4245cda76f954c03521fa2d5b82f2dd9
Akmal fudzni <akmalfudzni99@gmail.com>
2025-04-30 10:23:17 +0700

    Pesan commit, misalnya: update file digit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portofolio/', // harus sesuai dengan nama repo GitHub
   plugins: [react()],
-  base: '/portofolio/', 
 })
